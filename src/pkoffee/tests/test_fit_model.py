4a0bb54dd4992394bec182fb9fd8d76e37bdf85d
Ipek Guventurk <ipekguventurk@gmail.com>
2026-01-16 15:29:44 +0100

    Creation of a unit test file for following functions : predict, to_dict, from_dict and fit_model of the file fit_model.py (need to be completed)

---
+++ src/pkoffee/tests/test_fit_model.py
@@ -0,0 +1,21 @@
+import pytest
+'''
+def test_predict():
+    from pkoffee.fit_model import predict
+    import numpy as np
+
+    
+
+
+def test_to_dict():
+    from pkoffee.fit_model import to_dict
+    import numpy as np
+    
+def test_from_dict():
+    from pkoffee.fit_model import from_dict
+    import numpy as np
+    
+def test_fit_model():
+    from pkoffee.fit_model import fit_model
+    import numpy as np
+'''
